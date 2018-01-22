bf5bb8f400d6a813be1421a9f3762a97902b02ce
Cedric Champeau <cedric@gradle.com>
2018-01-22 12:03:31 +0100

    Try with 256m

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/ExcludeRuleMergingPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/ExcludeRuleMergingPerformanceTest.groovy
@@ -33,7 +33,7 @@ class ExcludeRuleMergingPerformanceTest extends AbstractCrossVersionPerformanceT
 
         given:
         runner.tasksToRun = ['resolveDependencies']
-        runner.gradleOpts = ["-Xms128m", "-Xmx128m"]
+        runner.gradleOpts = ["-Xms256m", "-Xmx256m"]
         runner.targetVersions = ["4.6-20180111105114+0000"]
         runner.args = ['-PuseHttp', "-PhttpPort=${serverPort}"]
 
@@ -53,7 +53,7 @@ class ExcludeRuleMergingPerformanceTest extends AbstractCrossVersionPerformanceT
 
         given:
         runner.tasksToRun = ['resolveDependencies']
-        runner.gradleOpts = ["-Xms128m", "-Xmx128m"]
+        runner.gradleOpts = ["-Xms256m", "-Xmx256m"]
         runner.args = ['-PuseHttp', "-PhttpPort=${serverPort}", "--parallel"]
         runner.targetVersions = ["4.6-20180111105114+0000"]
         when:
