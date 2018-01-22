eb02d3269cb25d5a4f10978d89007ed753bfdca3
Cedric Champeau <cedric@gradle.com>
2018-01-22 11:20:11 +0100

    Further reduce memory usage for performance scenario

--- subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/ExcludeRuleMergingPerformanceTest.groovy
+++ subprojects/performance/src/performanceTest/groovy/org/gradle/performance/regression/corefeature/ExcludeRuleMergingPerformanceTest.groovy
@@ -33,7 +33,7 @@ class ExcludeRuleMergingPerformanceTest extends AbstractCrossVersionPerformanceT
 
         given:
         runner.tasksToRun = ['resolveDependencies']
-        runner.gradleOpts = ["-Xms600m", "-Xmx600m"]
+        runner.gradleOpts = ["-Xms512m", "-Xmx512m"]
         runner.targetVersions = ["4.6-20180111105114+0000"]
         runner.args = ['-PuseHttp', "-PhttpPort=${serverPort}"]
 
@@ -53,7 +53,7 @@ class ExcludeRuleMergingPerformanceTest extends AbstractCrossVersionPerformanceT
 
         given:
         runner.tasksToRun = ['resolveDependencies']
-        runner.gradleOpts = ["-Xms600m", "-Xmx600m"]
+        runner.gradleOpts = ["-Xms512m", "-Xmx512m"]
         runner.args = ['-PuseHttp', "-PhttpPort=${serverPort}", "--parallel"]
         runner.targetVersions = ["4.6-20180111105114+0000"]
         when:
